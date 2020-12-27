149599d9637ae5d5fd31cbf3fdbe1a500d1d2747
SAYAN MANNA <73745559+smanna1504@users.noreply.github.com>
2020-12-27 16:46:57 +0530

    Tower Siege-3

---
+++ Block.js
@@ -0,0 +1,49 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+         
+          restitution :0.4,
+          friction :0.002,
+          //isStatic:true
+         
+      }
+      
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      if(this.body.speed<3){
+      var angle = this.body.angle;
+      var pos= this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      rect(0,0,this.width, this.height);
+      pop();
+      this.Visibility=255;
+      }
+
+      else{
+        World.remove(world,this.body);
+        push();
+        this.Visibility-=5;
+        tint(255,this.Visibility);
+        //translate(pos.x, pos.y);
+      //rotate(angle);
+      //rectMode(CENTER);
+      //rect(0,0,this.width, this.height);
+        pop();
+      }
+    }
+
+    score(){
+      if(this.Visibility<0 && this.Visibility>-300){
+    score++;
+      }
+    }
+
+    }
+
